6607198e1eb443492b4a21b36b1b40e818455f95
Aisha Evering <shovon3000g@gmail.com>
2023-10-24 23:05:40 -0700

    added waves

--- bored/src/App.js
+++ bored/src/App.js
@@ -4,6 +4,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">Bored?</header>
+      <section>
+        <div className="wave wave1"></div>
+        <div className="wave wave2"></div>
+        <div className="wave wave3"></div>
+        <div className="wave wave4"></div>
+      </section>
+      <div className="form-section">
+        <p>Questions and Results Go Here</p>
+      </div>
     </div>
   );
 }
